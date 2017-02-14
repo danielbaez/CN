6c7476c2cb9e9c1bd56a939c14e64de6f07c6b72
Daniel Baez <daniel_bg19@hotmail.com>
2017-02-13 23:59:01 -0500

    proyecto inicial

---
+++ app/Http/routes.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// User dependency injection
+Route::bind('user', function($user){
+    return App\User::find($user);
+});
+
+Route::get('auth/login', [
+	'as' => 'login-get',
+	'uses' => 'Auth\AuthController@getLogin'
+	]);
+
+Route::post('auth/login', [
+	'as' => 'login-post',
+	'uses' => 'Auth\AuthController@postLogin'
+]);
+
+Route::get('auth/logout', [
+	'as' => 'logout',
+	'uses' => 'Auth\AuthController@getLogout'
+]);
+
+Route::group(['middleware' => ['auth', 'no-cache'], 'prefix'=>'admin'], function () {
+    
+	Route::get('/', ['as' => 'home','uses' => 'HomeController@home']);	
+
+	// Route::group( ['middleware' => ['administrador']], function() {
+	// 	Route::resource('user', 'UserController');	
+	// });
+
+	// Route::group( ['middleware' => ['agente']], function() {
+	// 	Route::get('/calls', ['as' => 'calls', 'uses' => 'CallController@index']);
+	// 	Route::get('/repcot', ['as' => 'repcot', 'uses' => 'CallController@repcot']);
+	// 	Route::post('/calls/operation', ['as' => 'calls-operation', 'uses' => 'CallController@operation']);
+	// 	Route::get('/calls/ajax', ['as' => 'calls-ajax', 'uses' => 'CallController@calllAjax']);
+	// });
+
+});
---
+++ resources/views/Auth/login.blade.php
@@ -0,0 +1,36 @@
+@extends('auth.template')
+@section('content')
+	<div class="container text-center page-login">
+		<img class="logo" src="" alt="">
+
+		<div class="row container-login">
+			@include('partials.errors')
+			<form class="form-horizontal" method="POST" action="{{ route('login-post') }}" novalidate>
+				{!! csrf_field() !!}
+			    <div class="form-group">
+			      
+			      <div class="col-sm-12">
+			        <input type="text" name="usuario" class="form-control" placeholder="Ingrese su usuario" value="{{ old('usuario') }}">
+			      </div>
+			    </div>
+			    <div class="form-group">
+			      
+			      <div class="col-sm-12">
+			        <input type="password" name="password" class="form-control" placeholder="Ingrese su contraseña">
+			      </div>
+			    </div>
+			    <div class="form-group">
+			      
+			      <div class="col-sm-12">
+			        <input type="checkbox" name="remember"> Remember Me
+			      </div>
+			    </div>
+			    <div class="form-group">
+			      <div class="col-sm-12">
+			        <button type="submit" class="btn btn-primary">Ingresar</button>
+			      </div>
+			    </div>
+			</form>
+		</div>
+	</div>
+@stop
